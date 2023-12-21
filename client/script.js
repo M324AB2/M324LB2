485981d0d481f595d412a0dbb3474dc9f9cb6b1b
shanthosh04 <shanthosh1610@gmail.com>
2023-12-21 23:55:33 +0100

    add user write

--- client/script.js
+++ client/script.js
@@ -5,57 +5,74 @@
 
   const socket = new WebSocket(generateBackendUrl());
   socket.addEventListener('open', () => {
-    console.log('WebSocket connected!');
-    socket.send(JSON.stringify({ type: 'newUser', user: myUser }));
+      console.log('WebSocket connected!');
+      socket.send(JSON.stringify({ type: 'newUser', user: myUser }));
   });
   socket.addEventListener('message', (event) => {
-    const message = JSON.parse(event.data);
-    console.log('WebSocket message:', message);
-    switch (message.type) {
-      case 'message':
-        const messageElement = generateMessage(message, myUser);
-        document.getElementById('messages').appendChild(messageElement);
-        setTimeout(() => {
-          messageElement.classList.add('opacity-100');
-        }, 100);
-        break;
-      case 'activeUsers':
-        activeUsers = message.users;
-        break;
-      case 'typing':
-        typingUsers = message.users;
-        break;
-      default:
-        break;
-    }
+      const message = JSON.parse(event.data);
+      console.log('WebSocket message:', message);
+      switch (message.type) {
+          case 'message':
+              const messageElement = generateMessage(message, myUser);
+              document.getElementById('messages').appendChild(messageElement);
+              setTimeout(() => {
+                  messageElement.classList.add('opacity-100');
+              }, 100);
+              break;
+          case 'activeUsers':
+              activeUsers = message.users;
+              break;
+          case 'typing':
+              typingUsers = message.users;
+              break;
+          default:
+              break;
+      }
   });
   socket.addEventListener('close', (event) => {
-    console.log('WebSocket closed.');
+      console.log('WebSocket closed.');
   });
   socket.addEventListener('error', (event) => {
-    console.error('WebSocket error:', event);
+      console.error('WebSocket error:', event);
   });
 
-  // Wait until the DOM is loaded before adding event listeners
+ 
   document.addEventListener('DOMContentLoaded', (event) => {
-    // Send a message when the send button is clicked
-    document.getElementById('sendButton').addEventListener('click', () => {
-      const message = document.getElementById('messageInput').value;
-      socket.send(JSON.stringify({ type: 'message', message, user: myUser }));
-      document.getElementById('messageInput').value = '';
-    });
+      
+      document.getElementById('sendButton').addEventListener('click', () => {
+          sendMessage();
+      });
+
+      document.getElementById('messageInput').addEventListener('keydown', (event) => {
+          handleTyping(event);
+      });
   });
 
-  document.addEventListener('keydown', (event) => {
-    // Only send if the typed in key is not a modifier key
-    if (event.key.length === 1) {
-      socket.send(JSON.stringify({ type: 'typing', user: myUser }));
-    }
-    // Only send if the typed in key is the enter key
-    if (event.key === 'Enter') {
-      const message = document.getElementById('messageInput').value;
+  function sendMessage() {
+      const messageInput = document.getElementById('messageInput');
+      const message = messageInput.value;
       socket.send(JSON.stringify({ type: 'message', message, user: myUser }));
-      document.getElementById('messageInput').value = '';
-    }
-  });
+      messageInput.value = '';
+      hideTypingIndicator();
+  }
+
+  function handleTyping(event) {
+      const typingIndicator = document.getElementById('typingIndicator');
+
+      
+      if (event.key.length === 1) {
+          socket.send(JSON.stringify({ type: 'typing', user: myUser }));
+          typingIndicator.style.display = 'block'; 
+      }
+
+     
+      if (event.key === 'Enter') {
+          sendMessage();
+      }
+  }
+
+  function hideTypingIndicator() {
+      const typingIndicator = document.getElementById('typingIndicator');
+      typingIndicator.style.display = 'none'; 
+  }
 })();
