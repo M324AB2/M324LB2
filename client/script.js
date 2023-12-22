0beb2e487f717ba3f98bc674244326b0c1ef0acb
shanthosh04 <shanthosh1610@gmail.com>
2023-12-22 19:28:26 +0100

    change user typing

--- client/script.js
+++ client/script.js
@@ -1,109 +1,113 @@
 (async () => {
-    const myUser = await generateRandomUser();
-    let activeUsers = [];
-    let typingUsers = [];
-  
-    const socket = new WebSocket(generateBackendUrl());
-    socket.addEventListener('open', () => {
-      console.log('WebSocket connected!');
-      socket.send(JSON.stringify({ type: 'newUser', user: myUser }));
-    });
-    socket.addEventListener('message', (event) => {
-      const message = JSON.parse(event.data);
-      console.log('WebSocket message:', message);
-      switch (message.type) {
-        case 'message':
-          const messageElement = generateMessage(message, myUser);
-          document.getElementById('messages').appendChild(messageElement);
-          setTimeout(() => {
-            messageElement.classList.add('opacity-100');
-          }, 100);
-          break;
-        case 'activeUsers':
-          activeUsers = message.users;
-          break;
-        case 'typing':
-          typingUsers = message.users;
-          updateTypingIndicator(typingUsers);
-          break;
-        default:
-          break;
-      }
-    });
-    socket.addEventListener('close', (event) => {
-      console.log('WebSocket closed.');
-    });
-    socket.addEventListener('error', (event) => {
-      console.error('WebSocket error:', event);
-    });
+  const myUser = await generateRandomUser();
+  let activeUsers = [];
+  let typingUsers = [];
 
-  
-    // Wait until the DOM is loaded before adding event listeners
-    document.addEventListener('DOMContentLoaded', (event) => {
-      // Send a message when the send button is clicked
-      document.getElementById('sendButton').addEventListener('click', sendChatMessage);
-      document.getElementById('messageInput').addEventListener('keydown', handleKeyDown);
-      document.getElementById('messageInput').addEventListener('input', handleTyping);
-      const themeToggleButton = document.getElementById('themeToggle');
-          let isDarkMode = false;
-    });
-  
-    function sendChatMessage() {
-      const message = document.getElementById('messageInput').value;
-      if (message) {
-        socket.send(JSON.stringify({ type: 'message', message, user: myUser }));
-        document.getElementById('messageInput').value = '';
-        updateTypingIndicator([]);
-      }
-          themeToggleButton.addEventListener('click', () => {
-        isDarkMode = !isDarkMode;
-        if (isDarkMode) {
-            document.body.classList.add('dark-mode');
-            themeToggleButton.textContent = 'Light Mode';
-        } else {
-            document.body.classList.remove('dark-mode');
-            themeToggleButton.textContent = 'Dark Mode';
-        }
+  const socket = new WebSocket(generateBackendUrl());
+
+  // WebSocket open event
+  socket.addEventListener('open', () => {
+    console.log('WebSocket connected!');
+    socket.send(JSON.stringify({ type: 'newUser', user: myUser }));
   });
 
-
-
- 
-  
-
-
-
-  document.addEventListener('keydown', (event) => {
-    // Only send if the typed in key is not a modifier key
-    if (event.key.length === 1) {
-      socket.send(JSON.stringify({ type: 'typing', user: myUser }));
+  // WebSocket message event
+  socket.addEventListener('message', (event) => {
+    const message = JSON.parse(event.data);
+    console.log('WebSocket message:', message);
+    switch (message.type) {
+      case 'message':
+        const messageElement = generateMessage(message, myUser);
+        document.getElementById('messages').appendChild(messageElement);
+        setTimeout(() => {
+          messageElement.classList.add('opacity-100');
+        }, 100);
+        break;
+      case 'activeUsers':
+        activeUsers = message.users;
+        updateActiveUsersList(activeUsers);
+        break;
+      case 'typing':
+        typingUsers = message.users;
+        updateTypingIndicator();
+        break;
+      default:
+        break;
     }
-  
-    function handleKeyDown(event) {
-      // Send message on Enter key
+  });
+
+  // WebSocket close event
+  socket.addEventListener('close', () => {
+    console.log('WebSocket closed.');
+    const usersList = document.getElementById('activeUsers');
+    const parentElement = usersList.parentNode;
+    parentElement.removeChild(usersList);
+  });
+
+  // WebSocket error event
+  socket.addEventListener('error', (event) => {
+    console.error('WebSocket error:', event);
+  });
+
+  // Update active users list
+  const updateActiveUsersList = (users) => {
+    const usersList = document.getElementById('activeUsers');
+    usersList.innerHTML = '';
+    users.forEach(user => {
+      const userElement = document.createElement('li');
+      userElement.textContent = user.name;
+      usersList.appendChild(userElement);
+    });
+  };
+
+  // Update typing indicator display
+  const updateTypingIndicator = () => {
+    const typingIndicator = document.getElementById('typingIndicator');
+    if (typingUsers.length > 0) {
+      const userNames = typingUsers.map(user => user.name).join(', ');
+      typingIndicator.textContent = `${userNames} ${typingUsers.length > 1 ? 'are' : 'is'} typing...`;
+    } else {
+      typingIndicator.textContent = '';
+    }
+  };
+
+  // Debounce function to limit the frequency of typing status messages
+  const debounce = (func, delay) => {
+    let debounceTimer;
+    return function() {
+      const context = this, args = arguments;
+      clearTimeout(debounceTimer);
+      debounceTimer = setTimeout(() => func.apply(context, args), delay);
+    };
+  };
+
+  // Function to send typing status
+  const sendTypingStatus = debounce(() => {
+    socket.send(JSON.stringify({ type: 'typing', user: myUser }));
+  }, 500);
+
+  // Event listeners
+  document.addEventListener('DOMContentLoaded', () => {
+    // Send message button
+    document.getElementById('sendButton').addEventListener('click', sendMessage);
+
+    // Enter key to send message and typing event listener
+    document.getElementById('messageInput').addEventListener('keydown', (event) => {
       if (event.key === 'Enter') {
-        event.preventDefault();
-        sendChatMessage();
+        sendMessage();
+      } else if (event.key.length === 1 || event.key === 'Backspace') {
+        sendTypingStatus();
       }
-    }
-  
-    function handleTyping() {
-      const message = document.getElementById('messageInput').value;
-      if (message.length > 0) {
-        socket.send(JSON.stringify({ type: 'typing', user: myUser }));
-      }
-    }
-  
-    function updateTypingIndicator(users) {
-      const typingDiv = document.getElementById('typingIndicator');
-      if (users.length > 0) {
-        const names = users.map(u => u.name).join(', ');
-        typingDiv.textContent = `${names} is typing...`;
-      } else {
-        typingDiv.textContent = '';
-      }
-    }
-  
-  })();
-  
+    });
+  });
 
+  // Send message function
+  const sendMessage = () => {
+    const messageInput = document.getElementById('messageInput');
+    const message = messageInput.value.trim();
+    if (message) {
+      socket.send(JSON.stringify({ type: 'message', message, user: myUser }));
+      messageInput.value = '';
+    }
+  };
+})();
