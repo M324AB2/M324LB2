7b8916d9e258ca1a1a27d93af329666c2225909a
shanthosh04 <shanthosh1610@gmail.com>
2023-12-22 21:02:48 +0100

    change user typing

--- client/script.js
+++ client/script.js
@@ -4,14 +4,11 @@
   let typingUsers = [];
 
   const socket = new WebSocket(generateBackendUrl());
-
-  // WebSocket open event
   socket.addEventListener('open', () => {
     console.log('WebSocket connected!');
     socket.send(JSON.stringify({ type: 'newUser', user: myUser }));
   });
 
-  // WebSocket message event
   socket.addEventListener('message', (event) => {
     const message = JSON.parse(event.data);
     console.log('WebSocket message:', message);
@@ -29,14 +26,13 @@
         break;
       case 'typing':
         typingUsers = message.users;
-        updateTypingIndicator();
+        updateTypingUsers(typingUsers);
         break;
       default:
         break;
     }
   });
 
-  // WebSocket close event
   socket.addEventListener('close', () => {
     console.log('WebSocket closed.');
     const usersList = document.getElementById('activeUsers');
@@ -44,15 +40,13 @@
     parentElement.removeChild(usersList);
   });
 
-  // WebSocket error event
   socket.addEventListener('error', (event) => {
     console.error('WebSocket error:', event);
   });
 
-  // Update active users list
   const updateActiveUsersList = (users) => {
     const usersList = document.getElementById('activeUsers');
-    usersList.innerHTML = '';
+    usersList.innerHTML = ''; // Leere die Liste zuerst
     users.forEach(user => {
       const userElement = document.createElement('li');
       userElement.textContent = user.name;
@@ -60,54 +54,35 @@
     });
   };
 
-  // Update typing indicator display
-  const updateTypingIndicator = () => {
-    const typingIndicator = document.getElementById('typingIndicator');
-    if (typingUsers.length > 0) {
-      const userNames = typingUsers.map(user => user.name).join(', ');
-      typingIndicator.textContent = `${userNames} ${typingUsers.length > 1 ? 'are' : 'is'} typing...`;
-    } else {
-      typingIndicator.textContent = '';
-    }
+  const updateTypingUsers = (users) => {
+    const typingElement = document.getElementById('typingUsers');
+    typingElement.textContent = users.length > 0 
+      ? `${users.map(u => u.name).join(', ')} is/are typing...` 
+      : '';
   };
 
-  // Debounce function to limit the frequency of typing status messages
-  const debounce = (func, delay) => {
-    let debounceTimer;
-    return function() {
-      const context = this, args = arguments;
-      clearTimeout(debounceTimer);
-      debounceTimer = setTimeout(() => func.apply(context, args), delay);
-    };
-  };
-
-  // Function to send typing status
-  const sendTypingStatus = debounce(() => {
-    socket.send(JSON.stringify({ type: 'typing', user: myUser }));
-  }, 500);
-
-  // Event listeners
+  // Wait until the DOM is loaded before adding event listeners
   document.addEventListener('DOMContentLoaded', () => {
-    // Send message button
-    document.getElementById('sendButton').addEventListener('click', sendMessage);
+    const themeToggle = document.getElementById('themeToggle');
+    themeToggle.addEventListener('change', (event) => {
+      document.body.classList.toggle('dark-mode', event.target.checked);
+    });
 
-    // Enter key to send message and typing event listener
-    document.getElementById('messageInput').addEventListener('keydown', (event) => {
+    document.getElementById('sendButton').addEventListener('click', () => {
+      const message = document.getElementById('messageInput').value;
+      socket.send(JSON.stringify({ type: 'message', message, user: myUser }));
+      document.getElementById('messageInput').value = '';
+    });
+
+    document.addEventListener('keydown', (event) => {
+      if (event.key.length === 1) {
+        socket.send(JSON.stringify({ type: 'typing', user: myUser }));
+      }
       if (event.key === 'Enter') {
-        sendMessage();
-      } else if (event.key.length === 1 || event.key === 'Backspace') {
-        sendTypingStatus();
+        const message = document.getElementById('messageInput').value;
+        socket.send(JSON.stringify({ type: 'message', message, user: myUser }));
+        document.getElementById('messageInput').value = '';
       }
     });
   });
-
-  // Send message function
-  const sendMessage = () => {
-    const messageInput = document.getElementById('messageInput');
-    const message = messageInput.value.trim();
-    if (message) {
-      socket.send(JSON.stringify({ type: 'message', message, user: myUser }));
-      messageInput.value = '';
-    }
-  };
 })();
